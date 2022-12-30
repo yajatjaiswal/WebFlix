4f63613a28a7dfb5cc877229f56e71413d3b7238
Vinayak Porwal <84093231+VinayakPorwal@users.noreply.github.com>
2022-12-30 11:53:35 +0530

    Add files via upload

---
+++ src/components/MovieList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../Stylesheet/MoviList.css";
+
+const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        //here index is not used so i added it here using key
+        <div className="image-container" key={index}>
+          <div className="row">
+            <img src={movie.Poster} alt="movie"></img>
+            <div
+              onClick={() => props.handleFavouritesClick(movie)}
+              className="overlay d-flex align-items-center justify-content-center"
+            >
+              <FavouriteComponent />
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
